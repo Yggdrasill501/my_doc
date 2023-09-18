416c6cf104a4846b706db4c8feb00d26dda971ad
Filip Žitný <filipzitny@gmail.com>
2023-09-19 00:14:42 +0200

    views handeling

--- my_doc/api/views.py
+++ my_doc/api/views.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+"""File that handels request and response from webserver."""
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def main(request):
+    """Function that returns http respones after request.
+    
+    :param request:
+    :rtype object:
+    """
+    return HttpResponse("Response")
